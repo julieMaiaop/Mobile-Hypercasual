e198fd3d37827017eb95b4789f28a5e9b6c07105
julieMaiaop <167346442+julieMaiaop@users.noreply.github.com>
2025-09-04 08:31:25 -0300

    cdcfes

--- Mobile_Hypercasual/Assets/Scripts/ButtonHandler.cs
+++ Mobile_Hypercasual/Assets/Scripts/ButtonHandler.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ButtonHandler : MonoBehaviour
 {
+
     public void GoToFinal()
     {
         SceneManager.LoadScene("Final"); // Nome da sua cena final
+        Debug.Log("Mudando para a cena Final...");
+     
     }
 }
